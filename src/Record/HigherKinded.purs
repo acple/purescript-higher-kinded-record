4d22abe82a7568f817de14dfc923d2b51c47c17d
acple <silentsphere110@gmail.com>
2020-01-31 09:29:17 +0900

    merge modules

--- src/Record/HigherKinded.purs
+++ src/Record/HigherKinded.purs
@@ -1,11 +1,88 @@
-module Record.HigherKinded
-  ( module Record.HigherKinded.Functor
-  , module Record.HigherKinded.Lift
-  , module Record.HigherKinded.Traversable
-  , module Record.HigherKinded.Zip
-  ) where
+module Record.HigherKinded where
 
-import Record.HigherKinded.Functor (class RecordFunctor, mapRecord)
-import Record.HigherKinded.Lift (class LiftRecord, liftRecord, liftRecord', unliftRecord, unliftRecord')
-import Record.HigherKinded.Traversable (class RecordTraversable, foldMapRecord, sequenceRecord, traverseRecord)
-import Record.HigherKinded.Zip (class ZipRecord, zipRecord, zipWithRecord)
+import Prelude
+
+import Control.Comonad (class Comonad, extract)
+import Data.Const (Const(..))
+import Data.Functor.Product (Product, product)
+import Data.Identity (Identity(..))
+import Data.Newtype (un)
+import Prim.RowList as RL
+import Record.Builder as Builder
+import Record.HigherKinded.Internal as Internal
+import Type.Data.RowList (RLProxy(..))
+
+----------------------------------------------------------------
+
+class RecordFunctor fr gr f g | fr -> f, gr -> g, fr g -> gr, gr f -> fr where
+  mapRecord :: (f ~> g) -> { | fr } -> { | gr }
+
+instance instanceRecordFunctor ::
+  ( RL.RowToList fr rowList
+  , Internal.RecordFunctorInternal rowList fr gr f g
+  ) => RecordFunctor fr gr f g where
+  mapRecord f = Builder.build $ Internal.mapRecordImpl rowList f
+    where
+    rowList = RLProxy :: RLProxy rowList
+
+----------------------------------------------------------------
+
+class LiftRecord r fr f | fr -> r f, r f -> fr where
+  liftRecord' :: (forall a. a -> f a) -> { | r } -> { | fr }
+
+instance instanceLiftRecord ::
+  ( RL.RowToList r rowList
+  , Internal.LiftRecordInternal rowList r fr f
+  ) => LiftRecord r fr f where
+  liftRecord' f = Builder.build $ Internal.liftRecordImpl rowList f
+    where
+    rowList = RLProxy :: RLProxy rowList
+
+liftRecord :: forall r fr f. LiftRecord r fr f => Applicative f => { | r } -> { | fr }
+liftRecord = liftRecord' pure
+
+unliftRecord' :: forall fr r f. RecordTraversable fr r f Identity => (forall a. f a -> a) -> { | fr } -> { | r }
+unliftRecord' f = un Identity <<< traverseRecord (pure <<< f)
+
+unliftRecord :: forall fr r f. RecordTraversable fr r f Identity => Comonad f => { | fr } -> { | r }
+unliftRecord = unliftRecord' extract
+
+----------------------------------------------------------------
+
+class RecordTraversable fr r f g | fr -> f, fr g -> r where
+  traverseRecord :: (f ~> g) -> { | fr } -> g { | r }
+
+instance instanceRecordTraversable ::
+  ( RL.RowToList fr rowList
+  , Internal.RecordTraversableInternal rowList fr r f g
+  , Applicative g
+  ) => RecordTraversable fr r f g where
+  traverseRecord f fr = Builder.build <@> {} <$> Internal.traverseRecordImpl rowList f fr
+    where
+    rowList = RLProxy :: RLProxy rowList
+
+sequenceRecord :: forall fr r f. RecordTraversable fr r f f => { | fr } -> f { | r }
+sequenceRecord = traverseRecord identity
+
+foldMapRecord
+  :: forall fr r f m. RecordTraversable fr r f (Const m) => Monoid m
+  => (forall a. f a -> m) -> { | fr } -> m
+foldMapRecord f = un Const <<< traverseRecord (Const <<< f)
+
+----------------------------------------------------------------
+
+class ZipRecord fr gr hr f g h | fr -> f, gr -> g, hr -> h, fr gr h -> hr where
+  zipWithRecord :: (forall a. f a -> g a -> h a) -> { | fr } -> { | gr } -> { | hr }
+
+instance instanceZipRecord ::
+  ( RL.RowToList fr rowListF
+  , RL.RowToList gr rowListG
+  , Internal.ZipRecordInternal rowListF rowListG fr gr hr f g h
+  ) => ZipRecord fr gr hr f g h where
+  zipWithRecord f fr gr = Builder.build <@> {} $ Internal.zipWithRecordImpl rowListF rowListG f fr gr
+    where
+    rowListF = RLProxy :: RLProxy rowListF
+    rowListG = RLProxy :: RLProxy rowListG
+
+zipRecord :: forall fr gr pr f g. ZipRecord fr gr pr f g (Product f g) => { | fr } -> { | gr } -> { | pr }
+zipRecord = zipWithRecord product
---
+++ src/Record/HigherKinded/Internal.purs
@@ -0,0 +1,97 @@
+module Record.HigherKinded.Internal where
+
+import Prelude
+
+import Data.Symbol (class IsSymbol, SProxy(..))
+import Prim.Row as Row
+import Prim.RowList as RL
+import Record as Record
+import Record.Builder as Builder
+import Type.Data.RowList (RLProxy(..))
+
+----------------------------------------------------------------
+
+class RecordFunctorInternal rowList fr gr f g | rowList -> f, rowList g -> gr, fr -> f, gr -> g where
+  mapRecordImpl :: RLProxy rowList -> (f ~> g) -> Builder.Builder { | fr } { | gr }
+
+instance nilRecordFunctor :: RecordFunctorInternal RL.Nil fr fr f g where
+  mapRecordImpl _ _ = identity
+
+instance consRecordFunctor ::
+  ( IsSymbol key
+  , Row.Cons key (f a) r gr'
+  , Row.Cons key (g a) r gr
+  , RecordFunctorInternal tail fr gr' f g
+  ) => RecordFunctorInternal (RL.Cons key (f a) tail) fr gr f g where
+  mapRecordImpl _ f = Builder.modify key f <<< mapRecordImpl tail f
+    where
+    key = SProxy :: SProxy key
+    tail = RLProxy :: RLProxy tail
+
+----------------------------------------------------------------
+
+class LiftRecordInternal rowList r fr f | rowList f -> fr, fr -> f where
+  liftRecordImpl :: RLProxy rowList -> (forall a. a -> f a) -> Builder.Builder { | r } { | fr }
+
+instance nilLiftRecord :: LiftRecordInternal RL.Nil r r f where
+  liftRecordImpl _ _ = identity
+
+instance consLiftRecord ::
+  ( IsSymbol key
+  , Row.Cons key a r' fr'
+  , Row.Cons key (f a) r' fr
+  , LiftRecordInternal tail r fr' f
+  ) => LiftRecordInternal (RL.Cons key a tail) r fr f where
+  liftRecordImpl _ f = Builder.modify key f <<< liftRecordImpl tail f
+    where
+    key = SProxy :: SProxy key
+    tail = RLProxy :: RLProxy tail
+
+----------------------------------------------------------------
+
+class RecordTraversableInternal rowList fr r f g | rowList -> f, rowList g -> r, fr -> f where
+  traverseRecordImpl :: RLProxy rowList -> (f ~> g) -> { | fr } -> g (Builder.Builder {} { | r })
+
+instance nilRecordTraversable :: Applicative g => RecordTraversableInternal RL.Nil fr () f g where
+  traverseRecordImpl _ _ _ = pure identity
+
+instance consRecordTraversable ::
+  ( IsSymbol key
+  , Row.Cons key (f a) fr' fr
+  , Row.Cons key a r' r
+  , Row.Lacks key r'
+  , RecordTraversableInternal tail fr r' f g
+  , Applicative g
+  ) => RecordTraversableInternal (RL.Cons key (f a) tail) fr r f g where
+  traverseRecordImpl _ f fr =
+    (<<<) <$> Builder.insert key <$> f (Record.get key fr) <*> traverseRecordImpl tail f fr
+    where
+    key = SProxy :: SProxy key
+    tail = RLProxy :: RLProxy tail
+
+----------------------------------------------------------------
+
+class ZipRecordInternal rowListF rowListG fr gr hr f g h
+  | rowListF -> f, rowListG -> g, rowListF rowListG h -> hr, fr -> f, gr -> g, hr -> h
+  where
+  zipWithRecordImpl
+    :: RLProxy rowListF -> RLProxy rowListG
+    -> (forall a. f a -> g a -> h a) -> { | fr } -> { | gr } -> Builder.Builder {} { | hr }
+
+instance nilZipRecord :: ZipRecordInternal RL.Nil RL.Nil fr gr () f g h where
+  zipWithRecordImpl _ _ _ _ _ = identity
+
+instance consZipRecord ::
+  ( IsSymbol key
+  , Row.Cons key (f a) fr' fr
+  , Row.Cons key (g a) gr' gr
+  , Row.Cons key (h a) hr' hr
+  , Row.Lacks key hr'
+  , ZipRecordInternal tailF tailG fr gr hr' f g h
+  ) => ZipRecordInternal (RL.Cons key (f a) tailF) (RL.Cons key (g a) tailG) fr gr hr f g h where
+  zipWithRecordImpl _ _ f fr gr =
+    Builder.insert key (f (Record.get key fr) (Record.get key gr)) <<< zipWithRecordImpl tailF tailG f fr gr
+    where
+    key = SProxy :: SProxy key
+    tailF = RLProxy :: RLProxy tailF
+    tailG = RLProxy :: RLProxy tailG
